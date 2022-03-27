bca4ff02598d194db9cf26869783406452281a80
aalligithub <ali_kick@yahoo.com>
2022-03-27 14:04:35 +0430

    Basic MyClasses class

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import selenium
+class MyClasses:
+    def __init__(self, className, classTime, classUrl, classDict):
+        self.className = className
+        self.classTime = classTime
+        self.classUrl = classUrl
+        self.classDict = {}
+
+    def addClass(self):
+        return
+    
+    def deleteClass(self):
+        return
+
+    def numOfClasses(self):
+        return self.classDict.len
