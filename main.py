628efaf8a632b26adef5e281407497ed0dded552
aalligithub <ali_kick@yahoo.com>
2022-03-27 14:18:59 +0430

    Added Build Class and mocked up functions

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 class MyClasses:
-    def __init__(self, className, classTime, classUrl, classDict):
-        self.className = className
-        self.classTime = classTime
-        self.classUrl = classUrl
-        self.classDict = {}
+    def __init__(self, class_name, class_time, class_url, class_dict):
+        self.class_name = class_name
+        self.class_time = class_time
+        self.class_url = class_url
+        self.class_dict = dict()
 
     def addClass(self):
-        return
+        pass
     
     def deleteClass(self):
-        return
+        pass
 
     def numOfClasses(self):
-        return self.classDict.len
+        return self.class_dict.len
+
+
+class Build:
+    def __init__(self, password_path, username_path, login_ur):
+        self.password_path = password_path
+        self.username_path = username_path
+        self.login_url = login_ur
+
+    def login(self):
+        pass
+
+    def check_time_till_class_start(self):
+        pass
+
+    def get_to_class(self):
+        pass
