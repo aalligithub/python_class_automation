0fd5563388bc8e4e47b43b6447c5a8a24d587209
aalligithub <ali_kick@yahoo.com>
2022-04-02 21:37:57 +0430

    Commit

--- encryption.py
+++ encryption.py
@@ -1,6 +1,7 @@
 from asyncore import write
 from distutils.fancy_getopt import wrap_text
 from configparser import ConfigParser
+from unittest import case
 import rsa
 
 config_object = ConfigParser()
@@ -8,15 +9,17 @@ encrypted_message = ''
 
 
 def make_keys():
-    publicKey, privateKey = rsa.newkeys(512)
+    print('\nMaking Private and Public keys...')
+    public_key, private_key = rsa.newkeys(512)
 
     priv_key_file = open("private_key.ini", 'w')
-    priv_key_file.write(privateKey.save_pkcs1().decode('utf-8'))
+    priv_key_file.write(private_key.save_pkcs1().decode('utf-8'))
     priv_key_file.close()
 
     pub_key_file = open("public_key.ini", 'w')
-    pub_key_file.write(publicKey.save_pkcs1().decode('utf-8'))
+    pub_key_file.write(public_key.save_pkcs1().decode('utf-8'))
     pub_key_file.close()
+    print('Keys successfuly made')
 
 
 def encrypt(message_toBe_encrypted):
@@ -25,12 +28,9 @@ def encrypt(message_toBe_encrypted):
 
     encrypted_message = rsa.encrypt(message_toBe_encrypted.encode(), publicKey)
 
-    print("\noriginal string: ", message_toBe_encrypted)
-    print("\nencrypted string: ", encrypted_message)
+    pub_key_file.close()
 
     return encrypted_message
-
-    pub_key_file.close()
 
 
 def decrypt(encrypted_message):
@@ -43,13 +43,25 @@ def decrypt(encrypted_message):
     priv_key_file.close()
 
 
-make_keys()
+if __name__ == "__main__":
+    priv_key_file = open("private_key.ini", 'r')
+    pub_key_file = open("public_key.ini", 'r')
 
-message_toBe_encrypted = input('\nPlease enter what you want to encrypt : ')
+    if priv_key_file.read() == '' or pub_key_file.read() == '':
+        make_keys()
 
-encrypted_message = encrypt(message_toBe_encrypted)
+    user_input = input('\nHello welcome to the Encryptor, for encrypting your PASSWORD enter [1], for USERNAME enter [2], to renew your keys enter [3]... ')
 
-decrypt(encrypted_message)
+    match user_input:
+        case 1:
+            pass
+            exit()
 
-# add console that only runs make_keys once
+    message_toBe_encrypted = input('\nPlease enter what you want to encrypt : ')
+
+    encrypted_message = encrypt(message_toBe_encrypted)
+
+    decrypt(encrypted_message)
+
+
 # use encryption in the main code
