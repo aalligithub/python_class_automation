441a5626264d105c63ac25a20cf5628f39ecbeae
aalligithub <ali_kick@yahoo.com>
2022-04-03 00:10:58 +0430

    Added encryption match case

--- encryption.py
+++ encryption.py
@@ -1,11 +1,11 @@
 from asyncore import write
 from distutils.fancy_getopt import wrap_text
 from configparser import ConfigParser
-from unittest import case
 import rsa
 
 config_object = ConfigParser()
 encrypted_message = ''
+user_input = ''
 
 
 def make_keys():
@@ -19,7 +19,7 @@ def make_keys():
     pub_key_file = open("public_key.ini", 'w')
     pub_key_file.write(public_key.save_pkcs1().decode('utf-8'))
     pub_key_file.close()
-    print('Keys successfuly made')
+    print('Keys successfuly made, DO NOT SHARE YOUR PRIVATE KEY!')
 
 
 def encrypt(message_toBe_encrypted):
@@ -43,27 +43,45 @@ def decrypt(encrypted_message):
     priv_key_file.close()
 
 
+def console(user_input):
+    match user_input:
+        case 1:
+            # password encrypt
+            user_input = input(
+                '\nPlease input the Password to begin encrypting (Your password will never be accessible and only decrypted with your private key) or press Ctrl + C to cancel')
+            encrypt(user_input)
+            print('\nYour password was successfuly Encrypted!')
+
+        case 2:
+            # encrypt username
+            user_input = input(
+                '\nPlease input the Username to begin encrypting (Your username will never be accessible and only decrypted with your private key) or press Ctrl + C to cancel')
+            encrypt(user_input)
+            print('\nYour Username was successfuly Encrypted!')
+
+        case 3:
+            # renew keys
+            make_keys()
+
+        case 4:
+            # exits
+            exit()
+
+        case _:
+            # default case
+            print('\nError : Invalid Response, Please re run encryption.py')
+
+
 if __name__ == "__main__":
     priv_key_file = open("private_key.ini", 'r')
     pub_key_file = open("public_key.ini", 'r')
 
     if priv_key_file.read() == '' or pub_key_file.read() == '':
+        print('\nNo keys detected, generating keys...')
         make_keys()
 
     user_input = input(
-        '\nHello welcome to the Encryptor, for encrypting your PASSWORD enter [1], for USERNAME enter [2], to renew your keys enter [3]... ')
+        '\nHello welcome to the Encryptor, for encrypting your PASSWORD enter [1], for USERNAME enter [2], to renew your keys enter [3] and enter [4] to exit... ')
+    console(user_input)
 
-    match user_input:
-        case 1:
-            pass
-            exit()
-
-    message_toBe_encrypted = input(
-        '\nPlease enter what you want to encrypt : ')
-
-    encrypted_message = encrypt(message_toBe_encrypted)
-
-    decrypt(encrypted_message)
-
-
-# use encryption in the main code
+# DECRYPT IS EXTERAL USE ONLY DONT RUN HERE
